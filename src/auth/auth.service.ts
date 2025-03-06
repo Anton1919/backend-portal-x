eef80a881f0ab04a06e3640972c97135b3dc7515
Anton1919 <bf3kenzo@mail.ru>
2025-03-06 17:36:17 +0300

    1. implemented login, logout method 2. tested successfully

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,27 +2,36 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common'
 import { RegisterDto } from './dto/register.dto'
 import { UserService } from '../user/user.service'
 import { AuthMethod, User } from '../../prisma/__generated__'
-import { Request } from 'express'
-
-const emailIsAlreadyExistMessage =
-  'Пользователь с таким email уже есть. Пожалуйста, используйте другой email или войдите в систему'
-
-const serverErrorExceptionMessage =
-  'Не удалось сохранить сессию. Проверьте, правильно ли настроены параметры сессии'
+import { Request, Response } from 'express'
+import { LoginDto } from './dto/login.dto'
+import { verify } from 'argon2'
+import { ConfigService } from '@nestjs/config'
+import {
+  EMAIL_EXISTS_MESSAGE,
+  LOGIN_NOT_FOUND_MESSAGE,
+  LOGIN_INVALID_PASSWORD_MESSAGE,
+  LOGOUT_FAILED_MESSAGE,
+  SESSION_SAVE_MESSAGE,
+} from './consts/auth.errors'
 
 @Injectable()
 export class AuthService {
-  public constructor(private readonly userService: UserService) {}
+  public constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService, // он позволит нам, как минимум, брать данные из файла .env
+  ) {}
 
   public async register(req: Request, dto: RegisterDto) {
     const isAlreadyExists = await this.userService.findByEmail(dto.email)
 
     if (isAlreadyExists) {
-      throw new ConflictException(emailIsAlreadyExistMessage)
+      throw new ConflictException(EMAIL_EXISTS_MESSAGE)
     }
 
     const newUser = await this.userService.create(
@@ -37,9 +46,34 @@ export class AuthService {
     return this.saveSession(req, newUser)
   }
 
-  public async login() {}
+  public async login(req: Request, dto: LoginDto) {
+    const user = await this.userService.findByEmail(dto.email)
 
-  public async logout() {}
+    // проверка !user.password нужна в случае если пользователь авторизовался через соц сети
+    if (!user || !user.password) {
+      throw new NotFoundException(LOGIN_NOT_FOUND_MESSAGE)
+    }
+
+    const isValidPassword = await verify(user.password, dto.password) // пароли из базы данных, и из параметров
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException(LOGIN_INVALID_PASSWORD_MESSAGE)
+    }
+
+    return this.saveSession(req, user)
+  }
+
+  public async logout(req: Request, res: Response): Promise<void> {
+    return new Promise((resolve, reject) => {
+      req.session.destroy((err) => {
+        if (err) {
+          return reject(new InternalServerErrorException(LOGOUT_FAILED_MESSAGE))
+        }
+        res.clearCookie(this.configService.getOrThrow<string>('SESSION_NAME'))
+        resolve()
+      })
+    })
+  }
 
   // для redis
   private async saveSession(req: Request, user: User) {
@@ -49,9 +83,7 @@ export class AuthService {
 
       req.session.save((err) => {
         if (err) {
-          return reject(
-            new InternalServerErrorException(serverErrorExceptionMessage),
-          )
+          return reject(new InternalServerErrorException(SESSION_SAVE_MESSAGE))
         }
 
         resolve({ user })

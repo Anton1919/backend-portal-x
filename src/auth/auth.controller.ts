68c901edb21b5f7745a554d0a1184e6f08c9b4c4
Anton1919 <bf3kenzo@mail.ru>
2025-03-06 11:49:39 +0300

    1. implemented register controller 2. successfully saved data to redis 3. tested register new user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+} from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { RegisterDto } from './dto/register.dto'
+import { Request } from 'express'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register') // декорато с адресом "register", чтоб url был таким /auth/register
+  @HttpCode(HttpStatus.OK)
+  public async register(@Req() req: Request, @Body() dto: RegisterDto) {
+    return this.authService.register(req, dto)
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common'
+import { RegisterDto } from './dto/register.dto'
+import { UserService } from '../user/user.service'
+import { AuthMethod, User } from '../../prisma/__generated__'
+import { Request } from 'express'
+
+const emailIsAlreadyExistMessage =
+  'Пользователь с таким email уже есть. Пожалуйста, используйте другой email или войдите в систему'
+
+const serverErrorExceptionMessage =
+  'Не удалось сохранить сессию. Проверьте, правильно ли настроены параметры сессии'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  public constructor(private readonly userService: UserService) {}
+
+  public async register(req: Request, dto: RegisterDto) {
+    const isAlreadyExists = await this.userService.findByEmail(dto.email)
+
+    if (isAlreadyExists) {
+      throw new ConflictException(emailIsAlreadyExistMessage)
+    }
+
+    const newUser = await this.userService.create(
+      dto.email,
+      dto.password,
+      dto.name,
+      '',
+      AuthMethod.CREDENTIALS,
+      false,
+    )
+
+    return this.saveSession(req, newUser)
+  }
+
+  public async login() {}
+
+  public async logout() {}
+
+  // для redis
+  private async saveSession(req: Request, user: User) {
+    return new Promise((resolve, reject) => {
+      // обрати внимание что session.userid доступен благодаря файлу express.session
+      req.session.userId = user.id
+
+      req.session.save((err) => {
+        if (err) {
+          return reject(
+            new InternalServerErrorException(serverErrorExceptionMessage),
+          )
+        }
+
+        resolve({ user })
+      })
+    })
+  }
+}
